3325755f0ed64d51551f1eb53a38c55ade2d012f
Julien Atanassian <julien.atanassian@gmail.com>
2021-02-06 18:08:46 -0500

    add searchbar and display search results

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <SearchJokes @search-text="searchText" />
     <Joke v-for="joke in jokes" :key="joke.id" :joke="joke.joke" :id="joke.id" />
   </div>
 </template>
@@ -7,14 +8,32 @@
 <script>
 import axios from "axios";
 import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
 
 export default {
   components: {
-    Joke
+    Joke,
+    SearchJokes
   },
   data() {
     return {
       jokes: []
+    }
+  },
+  methods: {
+    async searchText(text) {
+      const config = {
+      headers: {
+        "Accept": "application/json"
+      }
+    };
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config)
+
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error.response)
+    }
     }
   },
   async created() {
